7da6ba8b70b85a42cdb6870f6b6b63dea0463176
sagor650 <sagor8@gmail.com>
2023-10-09 15:26:17 +0600

    department

--- backend/application/views/Home/department.php
+++ backend/application/views/Home/department.php
@@ -1,4 +1,4 @@
-<section class="department-banner">
+<section class="department-banner" style="position: relative;top: -111px">
     <h5 class="title1 display-5">Let’s Defeat it</h5>
     <h1 class="title2 display-4">Together</h1>
 </section>
@@ -9,9 +9,9 @@
                 <div class="col-lg-4 col-md-6 col-sm-6 p-4   d-flex justify-content-center">
                     <div class="card service_card  p-4 " >
                   <span>
-                    <h1><i class="fa-solid fa-user-doctor" ></i></h1>
+                    <h1><i class="fa-solid fa-house-medical-flag" ></i></h1>
                     <div class="card-body">
-                      <h5 class="card-title">Thyroid</h5>
+                      <h5 class="card-title">Medical</h5>
                       <p class="card-text">Oncology</p>
                     </div>
                   </span>
@@ -20,9 +20,9 @@
                 <div class="col-lg-4 col-md-6 col-sm-6 p-4   d-flex justify-content-center">
                     <div class="card service_card  p-4 " >
                   <span>
-                    <h1><i class="fa-solid fa-user-doctor" ></i></h1>
+                    <h1><i class="fa-solid fa-circle-radiation" ></i></h1>
                     <div class="card-body">
-                      <h5 class="card-title">Lungs</h5>
+                      <h5 class="card-title">Radiation</h5>
                       <p class="card-text">Oncology</p>
                     </div>
                   </span>
@@ -31,9 +31,20 @@
                 <div class="col-lg-4 col-md-6 col-sm-6 p-4 d-flex justify-content-center">
                     <div class="card service_card  p-4 " >
                   <span>
-                    <h1><i class="fa-solid fa-user-doctor" ></i></h1>
+                    <h1><i class="fa-solid fa-scissors" ></i></h1>
                     <div class="card-body">
-                      <h5 class="card-title">Urinary Bladder</h5>
+                      <h5 class="card-title">Surgery & Allied</h5>
+                      <p class="card-text">Service</p>
+                    </div>
+                  </span>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 p-4 d-flex justify-content-center">
+                    <div class="card service_card  p-4 " >
+                  <span>
+                    <h1><i class="fa-solid fa-highlighter" ></i></h1>
+                    <div class="card-body">
+                      <h5 class="card-title">Surgical</h5>
                       <p class="card-text">Oncology</p>
                     </div>
                   </span>
@@ -42,10 +53,32 @@
                 <div class="col-lg-4 col-md-6 col-sm-6 p-4 d-flex justify-content-center">
                     <div class="card service_card  p-4 " >
                   <span>
-                    <h1><i class="fa-solid fa-user-doctor" ></i></h1>
+                    <h1><i class="fa-solid fa-pills" ></i></h1>
                     <div class="card-body">
-                      <h5 class="card-title">Pancreatic</h5>
-                      <p class="card-text">Cacner</p>
+                      <h5 class="card-title">Medicine & Allied</h5>
+                      <p class="card-text">Service</p>
+                    </div>
+                  </span>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 p-4 d-flex justify-content-center">
+                    <div class="card service_card  p-4 " >
+                  <span>
+                    <h1><i class="fa-solid fa-bed-pulse" ></i></h1>
+                    <div class="card-body">
+                      <h5 class="card-title">30 Bedded</h5>
+                      <p class="card-text">Chemotherapy Day Care</p>
+                    </div>
+                  </span>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 p-4 d-flex justify-content-center">
+                    <div class="card service_card  p-4 " >
+                  <span>
+                    <h1><i class="fa-solid fa-flask-vial" ></i></h1>
+                    <div class="card-body">
+                      <h5 class="card-title">Laboratory</h5>
+                      <p class="card-text">With latest Technology</p>
                     </div>
                   </span>
                     </div>
@@ -55,8 +88,8 @@
                   <span>
                     <h1><i class="fa-solid fa-user-doctor" ></i></h1>
                     <div class="card-body">
-                      <h5 class="card-title">Overian</h5>
-                      <p class="card-text">& Cervical Cancer</p>
+                      <h5 class="card-title">Liver</h5>
+                      <p class="card-text">Transplantation Centre</p>
                     </div>
                   </span>
                     </div>
@@ -66,41 +99,8 @@
                   <span>
                     <h1><i class="fa-solid fa-user-doctor" ></i></h1>
                     <div class="card-body">
-                      <h5 class="card-title">Colon</h5>
-                      <p class="card-text">Cancer</p>
-                    </div>
-                  </span>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-6 p-4 d-flex justify-content-center">
-                    <div class="card service_card  p-4 " >
-                  <span>
-                    <h1><i class="fa-solid fa-user-doctor" ></i></h1>
-                    <div class="card-body">
-                      <h5 class="card-title">Pancreatic</h5>
-                      <p class="card-text">Cacner</p>
-                    </div>
-                  </span>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-6 p-4 d-flex justify-content-center">
-                    <div class="card service_card  p-4 " >
-                  <span>
-                    <h1><i class="fa-solid fa-user-doctor" ></i></h1>
-                    <div class="card-body">
-                      <h5 class="card-title">Overian</h5>
-                      <p class="card-text">& Cervical Cancer</p>
-                    </div>
-                  </span>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-6 p-4 d-flex justify-content-center">
-                    <div class="card service_card  p-4 " >
-                  <span>
-                    <h1><i class="fa-solid fa-user-doctor" ></i></h1>
-                    <div class="card-body">
-                      <h5 class="card-title">Colon</h5>
-                      <p class="card-text">Cancer</p>
+                      <h5 class="card-title">Kidney</h5>
+                      <p class="card-text">Transplantation Centre</p>
                     </div>
                   </span>
                     </div>
